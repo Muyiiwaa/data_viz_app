55420fb850846a14aee67226e81b2fedef660827
Muyiwa <forevermuyii@gmail.com>
2024-03-28 22:51:29 +0100

    third stable commit

--- main.py
+++ main.py
@@ -1,56 +1,63 @@
-# importing the libraries 
-
-
-import pandas as pd
-import numpy as np
-import plotly.express as px
 import streamlit as st
-import time
-
-options= ['bar-chart','line chart', 'scatter chart']
-# set up file upload
-
-"""
-# VIZZ-APP
-### Learn a lot and quickly form your dataset with just one click. Making data visualisation fun again
-"""
-st.sidebar.title("VIZZ-APP")
+import pandas as pd
+import plotly.express as px
+from chart_insight import chart_dict
+from chart_insight import generate_plotly_chart
 
 
-uploaded_data = st.sidebar.file_uploader(label="xyz", label_visibility='hidden')
-try:
-    if st.sidebar.button(label='Upload', type= 'secondary'):
-        with st.spinner("uploading loading file.."):
-            time.sleep(3)
-        st.sidebar.success(body="Done!")
-except ValueError:
-    st.write("You Must upload a dataset")    
+# Function to load CSV file
+def load_data(file):
+    df = pd.read_csv(file)
+    return df
 
+# Function to select columns
+def select_columns(df):
+    columns = st.multiselect('Select Columns', df.columns)
+    return columns
 
-if st.button(label= 'Try it Out', type='primary'):
-    st.divider()
-    st.sidebar.divider()
-    st.sidebar.title("Now Choose your charts")
+# Function to display DataFrame
+def display_dataframe(df):
+    st.write("### DataFrame")
+    st.write(df)
+    
+def display_charts2(df, cols, chart):
+    fig = generate_plotly_chart(chart_type=chart, data=df, cols= cols)
+    st.plotly_chart(fig)
 
-df = pd.read_csv(uploaded_data)
-st.write(df.head(5))
+# Function to display charts
+def display_charts(df, columns):
+    for col in columns:
+        fig = px.histogram(df, x=col, title=f'{col} Distribution')
+        st.plotly_chart(fig)
 
-chart = st.sidebar.radio(label= "Charts",
-                         options= ['bar_chart','line chart','scatter chart'],
-                         index=None)
+# Main function
+def main():
+    st.title('THE SIMPLE ANALYST')
 
-if chart:
-    st.success(f"You chose {chart}")
-    st.divider()
-    col1, col2 = st.columns(2)
-    with col1:
-        x = st.text_input(label= "X")
-    with col2:
-        y = st.text_input(label= "Y")
-    if chart == 'bar_chart':
-        fig = px.histogram(data_frame=df, x = df[x], y = df[y],
-                           color= df[x],
-                           title= f"Bar Chart of {x} and {y}")
-        st.plotly_chart(fig)  
-else:
-    st.error(f'Choose a Chart')
+    # Upload CSV file
+    uploaded_file = st.sidebar.file_uploader("Upload CSV file", type=['csv'])
+
+    if uploaded_file is not None:
+        # Load CSV file
+        df = load_data(uploaded_file)
+
+        # Select columns
+        selected_columns = select_columns(df)
+        # Display DataFrame
+        show_data = st.sidebar.toggle(label='show dataset')
+        if show_data:
+            display_dataframe(df)
+        
+        # select chart types.
+        chart_type = st.sidebar.radio(label='Choose the chart type:',
+                                      options= list(chart_dict.keys()))
+        if chart_type:
+            st.success(f'''{chart_type} chart {chart_dict[chart_type].lower()}''')
+            if st.button(label=f"Show {chart_type}", type= 'primary'):
+                if selected_columns:
+                    # Display charts
+                    #display_charts(df, selected_columns)
+                    display_charts2(df, cols = selected_columns,chart=chart_type)
+
+if __name__ == "__main__":
+    main()
